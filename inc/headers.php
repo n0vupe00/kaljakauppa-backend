0c489887ba29a02a399977cbcf5d9dd5783a8236
Pekka Vuokila <n0vupe00@students.oamk.fi>
2021-11-11 12:48:36 +0200

    phpn aloitus2

--- inc/headers.php
+++ inc/headers.php
@@ -7,11 +7,11 @@ header('Access-Control-Max-Age: 3600');
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    if (isset( $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
     if (isset( $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Methods: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']}");
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 
     exit(0);
 }
